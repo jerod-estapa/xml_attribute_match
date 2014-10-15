9b214bf58abe88068deccd77bfe9efd2a1c4a27f
jest2733 <jerodestapa@gmail.com>
2014-10-14 21:05:18 -0500

    Combined writer object and csv writer

--- xmlattributecsv.py
+++ xmlattributecsv.py
@@ -6,7 +6,6 @@
 
 import xml.etree.ElementTree as ET
 import csv
-import sys
 
 #puts inspection report numbers in a list
 codes = ['CO2014001451','TX3YZ8HQE1X1','TX3YAEHQE15W','CO2013001399','LA0004746572',
@@ -32,6 +31,7 @@ for x in codes:
             last_name = primary_driver.attrib['last_name']
             cdl_number = primary_driver.attrib['License_number']
             li.append((first_name, last_name, cdl_number))
+            print first_name, last_name, cdl_number
 
     for node in tree.iter('crash'):
         if node.attrib['report_number'] == x:
@@ -41,13 +41,14 @@ for x in codes:
             last_name = driver.attrib['last_name']
             cdl_number = driver.attrib['license_number']
             li.append((first_name, last_name, cdl_number))
+            print first_name, last_name, cdl_number
 
 
 #open a file for writing, truncates file and populates with li[]
 with open('xmlmatch_report.csv', 'wb') as f:
     f.truncate()
     mywriter = csv.writer(f)
-    writer.writerows(li)
+    mywriter.writerows(li)
 
 #closes the file
-csv_out.close()
+f.close()
